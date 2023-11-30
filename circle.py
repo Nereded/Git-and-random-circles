b3148ab05976e37e1149336aa750a9ba6f45c64e
Nereded <matrkhin@bk.ru>
2023-12-01 01:10:44 +1000

    Создание основного класса.

--- circle.py
+++ circle.py
@@ -21,3 +21,23 @@ class CirclesView(QGraphicsView):
         circle = Circle(diameter, color)
         item = self.scene.addEllipse(0, 0, diameter, diameter, QPen(Qt.NoPen), QBrush(QColor(color)))
         item.setPos(random.randint(0, 300), random.randint(0, 300))
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Random Circles Generator')
+        self.setGeometry(100, 100, 400, 400)
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.button = QPushButton('Add Circle', self)
+        self.button.clicked.connect(self.add_circle)
+        self.layout.addWidget(self.button)
+
+        self.circles_view = CirclesView()
+        self.layout.addWidget(self.circles_view)
